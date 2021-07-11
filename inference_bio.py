736b00fb376233cb3922b19f1dc47bcfe5dff4cc
Kranthi7910 <55890639+Kranthi7910@users.noreply.github.com>
2021-07-11 14:08:42 -0400

    Update inference_bio.py

--- inference_bio.py
+++ inference_bio.py
@@ -7,14 +7,17 @@ tokenizer = AutoTokenizer.from_pretrained("./bio-bert/", max_len=512)
 
 model = AutoModelForMaskedLM.from_pretrained("./bio-bert/pytorch_model.bin", config=config)
 
+#fill-mask pipeline returns a list of most probable filled sequences, with their probabilities
 fill_mask = pipeline(
     "fill-mask",
     model=model,
     tokenizer=tokenizer
 )
-   
+
+#random test sentence with a masked token
 test_str = "The [MASK] providers would adhere to PRT principles."
 
+#print results
 print (" Input String : " , test_str.replace("[MASK]", "____")) 
 
 predictions = fill_mask(test_str)
