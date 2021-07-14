1a03553878f4ed84065a904753274a5ae21549b6
Kranthi7910 <55890639+Kranthi7910@users.noreply.github.com>
2021-07-14 14:38:30 -0400

    Delete pipeline.py

--- pipeline.py
+++
@@ -1,9 +0,0 @@
-from transformers import pipeline
-
-fill_mask = pipeline(
-    "fill-mask",
-    model="./julien-c/EsperBERTo-small",
-    tokenizer="./julien-c/EsperBERTo-small"
-)
-
-fill_mask("La suno <mask>.")
